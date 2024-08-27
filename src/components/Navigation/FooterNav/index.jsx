1985b01c0f677d7193fca695605ff362fd2ce0b3
Charlotte Valset <charlottevalset@gmail.com>
2024-08-27 11:44:14 +0200

    Adds footer and footernav component

---
+++ src/components/Navigation/FooterNav/index.jsx
@@ -0,0 +1,28 @@
+import Logo from "../../../assets/images/holidaze-logo.png";
+
+export const FooterNav = () => {
+  return (
+    <section className="bg-dark-blue">
+      <div className="">
+        <img src={Logo} className="w-24 mx-auto py-4" alt="Holidaze Logo" />
+      </div>
+      <div className="flex flex-row justify-center gap-5 mb-4">
+        <a href="https://www.twitter.com/">
+          {" "}
+          <i class="text-4xl text-light-blue hover:text-soft-pink fa-brands fa-square-twitter"></i>
+        </a>
+        <a href="https://www.instagram.com/">
+          {" "}
+          <i class="text-4xl text-light-blue hover:text-soft-pink fa-brands fa-square-instagram"></i>
+        </a>
+        <a href="https://www.facebook.com/">
+          {" "}
+          <i class="text-4xl text-light-blue hover:text-soft-pink fa-brands fa-square-facebook"></i>
+        </a>
+      </div>
+      <p className="text-light-blue text-center font-light pb-4">
+        Designed & developed by <span className="text-soft-pink hover:underline cursor-pointer">Charlotte Valset</span>
+      </p>
+    </section>
+  );
+};
