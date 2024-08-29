4baba036481ea3f8a6b2b7f6899a8c292010a1e2
Charlotte Valset <charlottevalset@gmail.com>
2024-08-29 07:10:13 +0200

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { Header } from "../src/components/Layout/Header";
-import { Home } from "./pages/Home/index";
+import { Profile } from "./pages/Profile/index";
 import { Footer } from "./components/Layout/Footer";
 
 function App() {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <div className="max-w-screen-2xl mx-auto">
+        <Header />
+        <Profile />
+        <Footer />
+      </div>
     </>
   );
 }
