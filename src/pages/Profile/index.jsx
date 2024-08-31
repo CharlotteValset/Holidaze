9f6f6edcbec05b90b40e6439f7a8c37c5ed30bb0
Charlotte Valset <charlottevalset@gmail.com>
2024-08-31 22:05:41 +0200

    Adds a textLink with modal to profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,16 +1,40 @@
+import React, { useState } from "react";
+
 import { ProfileInfo } from "../../components/ProfileInfo";
 import { PrimaryButton } from "../../components/Buttons/PrimaryButton";
+import { SecondaryButton } from "../../components/Buttons/SecondaryButton";
 import { ProfileTabs } from "../../components/ProfileTabs";
+import { TextLink } from "../../components/TextLink";
+import { Modal } from "../../components/Modal";
 
 export const Profile = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <>
-      <h1 className="mt-20 md:mt-28 text-2xl text-center">My profile</h1>
-      <div className="md:flex md:justify-between md:mx-auto md:max-w-4xl">
+      <h1 className="mt-20 text-2xl text-center sm:hidden">My profile</h1>
+      <div className="sm:flex sm:justify-between sm:mx-auto sm:max-w-screen-lg sm:mb-4 sm:mt-12 md:mt-20">
         <ProfileInfo />
-        <div className="flex justify-center mt-3 mb-6">
+        <div className="flex justify-center mt-6 mb-6 mr-2 sm:w-full sm:my-auto sm:justify-end sm:p-2">
           <PrimaryButton>+ Add new venue</PrimaryButton>
         </div>
+      </div>
+      <div className="flex justify-center mb-4">
+        <TextLink onClick={() => setIsModalOpen(true)}>Want to become a venue manager?</TextLink>{" "}
+        {isModalOpen && (
+          <Modal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            title="I want to become a venue manager"
+            footer={
+              <>
+                <SecondaryButton onClick={() => setIsModalOpen(false)}>No</SecondaryButton>
+                <PrimaryButton onClick={() => setIsModalOpen(false)} className="mx-5">
+                  Yes
+                </PrimaryButton>
+              </>
+            }
+          ></Modal>
+        )}
       </div>
       <ProfileTabs />
     </>
