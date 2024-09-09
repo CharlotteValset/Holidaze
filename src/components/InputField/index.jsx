f8eb1afeedb4c866c09d7db351badf61312f6348
Charlotte Valset <charlottevalset@gmail.com>
2024-09-09 14:47:03 +0200

    Adds toggle password function to <InputField>

--- src/components/InputField/index.jsx
+++ src/components/InputField/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const InputField = ({
   icon,
@@ -12,22 +12,40 @@ export const InputField = ({
   min,
   className,
   errors,
+  togglePassword = false,
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <div className="mx-auto my-4 flex w-60 flex-col">
       <label htmlFor={htmlFor} className="ps-1 sm:text-lg">
         {icon && <span className="mr-2">{icon}</span>} {label}
       </label>
-      <input
-        {...register(
-          registerYup,
-          required ? { required: "This field is required" } : {},
+      <div className="relative flex items-center">
+        <input
+          {...register(
+            registerYup,
+            required ? { required: "This field is required" } : {},
+          )}
+          id={id}
+          type={togglePassword ? (showPassword ? "text" : "password") : type}
+          min={min}
+          className={className}
+        />
+        {togglePassword && (
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="absolute right-10 text-xs text-dark-gray sm:right-3"
+          >
+            {showPassword ? "Hide" : "Show"}
+          </button>
         )}
-        id={id}
-        type={type}
-        min={min}
-        className={className}
-      />
+      </div>
       {errors && errors[registerYup] && (
         <p className="text-sm text-red-500">{errors[registerYup]?.message}</p>
       )}
