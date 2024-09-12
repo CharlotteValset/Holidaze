25fa9e0b26bcccf9f5fa3b79286ef76112fb861a
Charlotte Valset <charlottevalset@gmail.com>
2024-09-12 13:22:25 +0200

    adds constants & fetch functions/hooks

---
+++ src/js/api/constants.js
@@ -0,0 +1,11 @@
+export const API_Base_Url = "https://v2.api.noroff.dev";
+
+export const API_Url = `${API_Base_Url}/holidaze`;
+
+export const API_Auth = "/auth";
+export const API_Key = "942b0bef-3136-4c35-b9c3-190d76f103e8";
+
+export const all_Venues = "/venues";
+
+export const API_Register_Url = "/register";
+export const API_Login_Url = "/login";
