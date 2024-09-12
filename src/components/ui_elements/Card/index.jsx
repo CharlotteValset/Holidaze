34b3c129d66edac763357df28ec5c32d744f42f7
Charlotte Valset <charlottevalset@gmail.com>
2024-09-12 13:32:13 +0200

    adds data from api to <Card>

--- src/components/ui_elements/Card/index.jsx
+++ src/components/ui_elements/Card/index.jsx
@@ -1,22 +1,42 @@
+import React from "react";
 import { Link } from "react-router-dom";
-import HeroImage from "../../../assets/images/heroImage.png";
 import { PrimaryButton } from "../../ui_elements/Buttons/PrimaryButton";
+import ImagePlaceholder from "../../../assets/images/no_img.png";
 
-export const Card = () => {
+const updateImageUrlFromDomains = (media) => {
+  return media.url;
+};
+
+export const Card = ({ data }) => {
+  const imageArray = data.media;
+
+  const imageUrl =
+    Array.isArray(imageArray) &&
+    imageArray.length > 0 &&
+    imageArray[0] !== "null" &&
+    imageArray[0].url
+      ? updateImageUrlFromDomains(imageArray[0])
+      : ImagePlaceholder;
+
+  const location =
+    data.location?.city && data.location?.country
+      ? `${data.location.city}, ${data.location.country}`
+      : "Location not available";
+
   return (
     <article className="mx-auto my-1 w-11/12 rounded-xl bg-light-blue sm:w-72">
       <div className="flex flex-col">
         <div className="">
           <img
-            className="max-h-72 w-full rounded-t-xl object-cover"
-            src={HeroImage}
+            className="h-48 w-full rounded-t-xl object-cover"
+            src={imageUrl}
             alt="image"
           />
         </div>
         <div className="m-1 px-2">
           <div className="flex items-center justify-between">
             <h2 className="cursor-text truncate text-xl font-normal tracking-tight text-dark-blue">
-              Luxury Home
+              {data.name}
             </h2>
             <div className="flex items-center">
               <span className="flex flex-row items-center rounded px-2 py-0.5 text-xs font-normal text-dark-blue">
@@ -29,19 +49,20 @@ export const Card = () => {
                 >
                   <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                 </svg>{" "}
-                4.6
+                {data.rating}
               </span>
             </div>
           </div>
           <div className="flex gap-1">
             <i className="fa-solid fa-location-dot text-xs"></i>
-            <p className="text-xs">Bergen, Norway</p>
+            <p className="text-xs">{location}</p>
           </div>
           <div className="my-2 flex items-center justify-between">
             <p className="text-base font-medium">
-              $ 975 <span className="text-xs font-normal">per night</span>
+              $ {data.price}{" "}
+              <span className="text-xs font-normal">per night</span>
             </p>
-            <Link to="/singleVenue">
+            <Link to={`/singleVenue/${data.id}`}>
               <PrimaryButton>View more</PrimaryButton>
             </Link>
           </div>
