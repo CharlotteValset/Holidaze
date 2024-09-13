ea6927d02232558c8b2089352ab6806f810445b3
Charlotte Valset <charlottevalset@gmail.com>
2024-09-13 11:54:18 +0200

    adds endpoints

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -6,6 +6,8 @@ export const API_Auth = "/auth";
 export const API_Key = "942b0bef-3136-4c35-b9c3-190d76f103e8";
 
 export const all_Venues = "/venues";
+export const profile_Url = "/profiles";
+export const bookings_Url = "/bookings";
 
 export const API_Register_Url = "/register";
 export const API_Login_Url = "/login";
