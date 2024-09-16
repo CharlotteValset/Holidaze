ea2e67df2f7a3a10d13547e316be5102adce3f10
Charlotte Valset <charlottevalset@gmail.com>
2024-09-16 13:09:05 +0200

    displays cta if user is venue manager

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ProfileInfo } from "../../components/profile/ProfileInfo";
 import { PrimaryButton } from "../../components/ui_elements/Buttons/PrimaryButton";
@@ -9,13 +9,20 @@ import { load } from "../../js/storage/load";
 import { BecomeVenueManager } from "../../components/profile/BecomeVenueManager";
 
 export const Profile = () => {
-  const userProfile = load("profile");
+  const [isVenueManager, setIsVenueManager] = useState(false);
 
+  const userProfile = load("profile");
   const userId = userProfile?.name;
 
   const { data, isLoading, hasError } = useFetch(
     `${API_Url}${profile_Url}/${userId}?_bookings=true&_venues=true`,
   );
+
+  useEffect(() => {
+    if (data && data.venueManager !== undefined) {
+      setIsVenueManager(data.venueManager);
+    }
+  }, [data]);
 
   let content;
 
@@ -33,13 +40,15 @@ export const Profile = () => {
         <h1 className="mt-20 text-center text-2xl sm:hidden">My profile</h1>
         <div className="mx-auto flex w-full max-w-screen-lg flex-col sm:mt-16 sm:flex-row sm:justify-around md:mt-20">
           <ProfileInfo data={data} key={data} />
-          <Link
-            to="/addEditVenue"
-            aria-label="Add/edit venue"
-            className="mb-6 mr-2 mt-6 flex justify-center sm:my-auto sm:w-full sm:justify-end sm:p-2"
-          >
-            <PrimaryButton>+ Add new venue</PrimaryButton>
-          </Link>
+          {isVenueManager && (
+            <Link
+              to="/addEditVenue"
+              aria-label="Add/edit venue"
+              className="mb-6 mr-2 mt-6 flex justify-center sm:my-auto sm:w-full sm:justify-end sm:p-2"
+            >
+              <PrimaryButton>+ Add new venue</PrimaryButton>
+            </Link>
+          )}
         </div>
         <BecomeVenueManager />
         <ProfileTabs />
