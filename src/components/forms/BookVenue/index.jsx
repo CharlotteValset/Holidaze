c9172ae36465d361de2260c15096453be06e69a8
Charlotte Valset <charlottevalset@gmail.com>
2024-09-20 11:26:01 +0200

    Revert "test"
    This reverts commit b76d688cda0c5f546d83fb590300a42f0aea5612.

--- src/components/forms/BookVenue/index.jsx
+++ src/components/forms/BookVenue/index.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import DatePicker from "react-date-picker";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { PrimaryButton } from "../../ui_elements/Buttons/PrimaryButton";
+import { InputField } from "../../form_elements/InputField";
 import { BookVenueNotLoggedIn } from "../../../components/bookings/BookVenueNotLoggedIn";
 import { load } from "../../../js/storage/load";
-import { Flowbite, Datepicker } from "flowbite-react";
+
 const schema = yup
   .object({
     checkIn: yup
@@ -19,49 +20,11 @@ const schema = yup
   })
   .required();
 
-const customTheme = {
-  datepicker: {
-    root: {
-      base: "relative",
-    },
-    popup: {
-      root: {
-        base: "absolute top-10 z-50 block pt-2",
-        inline: "relative top-0 z-auto",
-        inner:
-          "inline-block rounded-lg bg-white p-4 shadow-lg dark:bg-gray-700",
-      },
-    },
-    input: {
-      base: "block w-full rounded-lg border-gray-300 bg-white p-2.5 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-700 dark:text-white", // Customize text color
-      text: "text-gray-900",
-    },
-    views: {
-      days: {
-        items: {
-          item: {
-            selected: "bg-cyan-700 text-blue hover:bg-cyan-600",
-          },
-        },
-      },
-    },
-    footer: {
-      button: {
-        today:
-          "bg-cyan-700 text-white hover:bg-cyan-800 dark:bg-cyan-600 dark:hover:bg-cyan-700",
-        clear:
-          "border border-gray-300 bg-white text-gray-900 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600",
-      },
-    },
-  },
-};
-
 export const BookVenue = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-    setValue,
     reset,
   } = useForm({ resolver: yupResolver(schema) });
 
@@ -71,48 +34,33 @@ export const BookVenue = () => {
   };
 
   const token = load("accessToken");
-  const [selectedDate, setSelectedDate] = useState(null);
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
 
   return (
     <div className="mx-auto mb-5 mt-8 h-fit w-11/12 rounded-lg bg-light-blue xs:mx-0 xs:ml-4 xs:max-w-xs">
       <h3 className="pb-2 pt-4 text-center text-xl">Book this venue!</h3>
       <form onSubmit={handleSubmit(onSubmit)} className="mx-auto w-48">
-        <Flowbite theme={{ theme: customTheme }}>
-          <div className="mx-auto mt-5 max-w-sm">
-            <Datepicker onChange={handleDateChange} value={selectedDate} />
-          </div>
-        </Flowbite>
-        <div className="mx-auto my-2 flex flex-col">
-          <label htmlFor="checkInDate">Check-in</label>
-          <Datepicker
-            weekStart={1}
-            {...register("checkIn", {
-              required: true,
-            })}
-          />
-          {errors.checkIn && (
-            <p className="text-sm text-red-500">{errors.checkIn.message}</p>
-          )}
-        </div>
-        <div className="mx-auto my-2 flex flex-col">
-          <label htmlFor="checkInDate">Check-out</label>
-          <Datepicker
-            onChange={(date) => handleDateChange("checkIn", date)}
-            className="selected:bg-red-500 selected:text-white hover:bg-red-400"
-          />
-          <Datepicker
-            weekStart={1}
-            {...register("checkOut", {
-              required: true,
-            })}
-          />
-          {errors.checkOut && (
-            <p className="text-sm text-red-500">{errors.checkOut.message}</p>
-          )}
-        </div>
+        <InputField
+          label="Check-in"
+          htmlFor="checkInDate"
+          register={register}
+          registerYup="checkIn"
+          required={true}
+          id="checkInDate"
+          type="date"
+          className="h-8 rounded-lg border-gray-300 text-center"
+          errors={errors}
+        />
+        <InputField
+          label="Check-out"
+          htmlFor="checkOutDate"
+          register={register}
+          registerYup="checkOut"
+          required={true}
+          id="checkOutDate"
+          type="date"
+          className="h-8 rounded-lg border-gray-300 text-center"
+          errors={errors}
+        />
         <div className="mx-auto my-2 flex flex-col">
           <label htmlFor="numberOfGuests" className="pb-1 ps-1 sm:text-lg">
             Number of guests
