4e3cdfcea061ddf76ae0d280f47af85a6ca7c6a0
Charlotte Valset <charlottevalset@gmail.com>
2024-09-23 11:37:14 +0200

    fixed image size

--- src/components/ui_elements/Gallery/index.jsx
+++ src/components/ui_elements/Gallery/index.jsx
@@ -19,7 +19,7 @@ export const Gallery = ({ data }) => {
     <div className="mx-auto my-5 w-11/12 gap-4">
       <div className="flex-1">
         <img
-          className="h-44 w-full rounded-lg object-cover xs:h-52 sm:h-80 md:h-96 lg:h-[500px]"
+          className="h-44 w-full rounded-lg object-cover xs:h-72 sm:h-80 md:h-96 lg:h-[500px]"
           src={largeImage}
           alt="Large venue image"
           onError={handleImageErrors}
